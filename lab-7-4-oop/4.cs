a422253d08bd8e9743c2eb8a51fccabe6458177b
maxlick <maxoriginalgame@gmail.com>
2023-11-06 13:00:01 +0200

    додано проєкт

---
+++ lab-7-4-oop/4.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7_4_oop
+{
+     //1
+    public class Calculator<T>
+    {
+        public delegate T OperationDelegate(T a, T b);
+
+        public OperationDelegate Add { get; set; }
+        public OperationDelegate Subtract { get; set; }
+        public OperationDelegate Multiply { get; set; }
+        public OperationDelegate Divide { get; set; }
+
+        public T PerformOperation(OperationDelegate operation, T a, T b)
+        {
+            return operation(a, b);
+        }
+    }
+
+     //2
+    public class Repository<T>
+    {
+        private List<T> items;
+
+        public Repository()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public List<T> Find(Criteria<T> criteria)
+        {
+            List<T> results = new List<T>();
+            foreach (T item in items)
+            {
+                if (criteria(item))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+    }
+
+    public delegate bool Criteria<T>(T item);
+
+     //3
+    public class FunctionCache<TKey, TResult>
+    {
+        private Dictionary<TKey, CacheItem<TResult>> cache;
+        private TimeSpan expirationTime;
+
+        public FunctionCache(TimeSpan expirationTime)
+        {
+            this.expirationTime = expirationTime;
+            cache = new Dictionary<TKey, CacheItem<TResult>>();
+        }
+
+        public TResult ExecuteFunction(Func<TKey, TResult> function, TKey key)
+        {
+            if (cache.ContainsKey(key))
+            {
+                var cacheItem = cache[key];
+                if ((DateTime.Now - cacheItem.TimeStamp) < expirationTime)
+                {
+                    return cacheItem.Result;
+                }
+            }
+
+            TResult result = function(key);
+            cache[key] = new CacheItem<TResult>(result, DateTime.Now);
+            return result;
+        }
+
+        private class CacheItem<T>
+        {
+            public T Result { get; }
+            public DateTime TimeStamp { get; }
+
+            public CacheItem(T result, DateTime timeStamp)
+            {
+                Result = result;
+                TimeStamp = timeStamp;
+            }
+        }
+    }
+
+     //4
+    public class TaskScheduler<TTask, TPriority>
+    {
+        private SortedDictionary<TPriority, Queue<TTask>> taskQueue;
+        private Func<TTask, TPriority> prioritySelector;
+        private Action<TTask> taskExecutor;
+
+        public TaskScheduler(Func<TTask, TPriority> prioritySelector, Action<TTask> taskExecutor)
+        {
+            this.prioritySelector = prioritySelector;
+            this.taskExecutor = taskExecutor;
+            taskQueue = new SortedDictionary<TPriority, Queue<TTask>>();
+        }
+
+        public void AddTask(TTask task)
+        {
+            TPriority priority = prioritySelector(task);
+            if (!taskQueue.ContainsKey(priority))
+            {
+                taskQueue[priority] = new Queue<TTask>();
+            }
+            taskQueue[priority].Enqueue(task);
+        }
+
+        public void ExecuteNext()
+        {
+            var highestPriority = taskQueue.Keys.GetEnumerator().MoveNext() ? taskQueue.Keys.GetEnumerator().Current : default(TPriority);
+            if (highestPriority != null && taskQueue[highestPriority].Count > 0)
+            {
+                TTask task = taskQueue[highestPriority].Dequeue();
+                taskExecutor(task);
+            }
+            else
+            {
+                Console.WriteLine("No tasks to execute.");
+            }
+        }
+    }
+}
---
+++ lab-7-4-oop/Program.cs
@@ -0,0 +1,86 @@
+﻿using lab_7_4_oop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1
+        Calculator<int> intCalc = new Calculator<int>();
+        intCalc.Add = (a, b) => a + b;
+        intCalc.Subtract = (a, b) => a - b;
+        intCalc.Multiply = (a, b) => a * b;
+        intCalc.Divide = (a, b) => b != 0 ? a / b : throw new ArgumentException("Cannot divide by zero.");
+
+        int intResultAdd = intCalc.PerformOperation(intCalc.Add, 3, 5);
+        int intResultSubtract = intCalc.PerformOperation(intCalc.Subtract, 5, 3);
+        int intResultMultiply = intCalc.PerformOperation(intCalc.Multiply, 3, 5);
+        int intResultDivide = intCalc.PerformOperation(intCalc.Divide, 10, 2);
+
+        Console.WriteLine("Integer calculations:");
+        Console.WriteLine($"Addition: {intResultAdd}");
+        Console.WriteLine($"Subtraction: {intResultSubtract}");
+        Console.WriteLine($"Multiplication: {intResultMultiply}");
+        Console.WriteLine($"Division: {intResultDivide}");
+
+        Calculator<double> doubleCalc = new Calculator<double>();
+        doubleCalc.Add = (a, b) => a + b;
+        doubleCalc.Subtract = (a, b) => a - b;
+        doubleCalc.Multiply = (a, b) => a * b;
+        doubleCalc.Divide = (a, b) => b != 0 ? a / b : throw new ArgumentException("Cannot divide by zero.");
+
+        double doubleResultAdd = doubleCalc.PerformOperation(doubleCalc.Add, 3.5, 4.8);
+        double doubleResultSubtract = doubleCalc.PerformOperation(doubleCalc.Subtract, 5.9, 3.8);
+        double doubleResultMultiply = doubleCalc.PerformOperation(doubleCalc.Multiply, 3.4, 4.8);
+        double doubleResultDivide = doubleCalc.PerformOperation(doubleCalc.Divide, 12.8, 4);
+
+        Console.WriteLine("\nDouble calculations:");
+        Console.WriteLine($"Addition: {doubleResultAdd}");
+        Console.WriteLine($"Subtraction: {doubleResultSubtract}");
+        Console.WriteLine($"Multiplication: {doubleResultMultiply}");
+        Console.WriteLine($"Division: {doubleResultDivide}");
+
+        //2
+        Repository<int> repository = new Repository<int>();
+        repository.Add(1);
+        repository.Add(2);
+        repository.Add(3);
+        repository.Add(4);
+        repository.Add(5);
+
+        List<int> evenNumbers = repository.Find(item => item % 2 == 0);
+        Console.WriteLine("\nEven numbers:");
+        foreach (var evenNumber in evenNumbers)
+        {
+            Console.WriteLine(evenNumber);
+        }
+
+        //3
+        FunctionCache<int, string> cache = new FunctionCache<int, string>(TimeSpan.FromMinutes(1));
+
+        Func<int, string> myFunction = (key) => "Value for key " + key;
+
+        string result1 = cache.ExecuteFunction(myFunction, 5);
+        Console.WriteLine(result1);
+
+        System.Threading.Thread.Sleep(61000);
+
+        string result2 = cache.ExecuteFunction(myFunction, 5);
+        Console.WriteLine(result2);
+
+        //4
+        TaskScheduler<string, int> scheduler = new TaskScheduler<string, int>(
+            task => task.Length,
+            task => Console.WriteLine("Executing task: " + task));
+
+        Console.WriteLine("Enter a task (or 'exit' to quit):");
+        string input = Console.ReadLine();
+        while (input != "exit")
+        {
+            scheduler.AddTask(input);
+            Console.WriteLine("Enter a task (or 'exit' to quit):");
+            input = Console.ReadLine();
+        }
+
+        scheduler.ExecuteNext();
+    }
+}
